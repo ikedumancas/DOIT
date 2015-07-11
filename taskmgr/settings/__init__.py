e712694ac7bd8d6c5d1a2ac83fd535c2ff2f4bd8
Miguel Ike Dumancas Jr <ikedumancas.official@gmail.com>
2015-07-11 15:05:29 +0800

    requirements.txt

--- taskmgr/settings/__init__.py
+++ taskmgr/settings/__init__.py
@@ -1,9 +1,11 @@
 import os
- 
+
 dev = os.environ.get('DJANGO_DEV')
 print dev
 if dev:
-    from .development import *
+	print 'dev'
+	from .development import *
 else:
-    from .heroku import *
+	print 'heroku'
+	from .heroku import *
 
