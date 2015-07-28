e01a95371b28084c08887725e3c129ba21f8edbc
Miguel Ike Dumancas Jr <ikedumancas.official@gmail.com>
2015-07-28 17:46:03 +0800

    add sample list on registration

--- account/models.py
+++ account/models.py
@@ -1,8 +1,9 @@
+import datetime
 import string
 from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
-from tasks.models import TodoList
+from tasks.models import Todo, TodoList
 
 # Create your models here.
 class UserProfile(models.Model):
@@ -37,8 +38,28 @@ def new_user_receiver(sender, instance, created, *args, **kwargs):
         # create user profile
         new_profile, is_created = UserProfile.objects.get_or_create(user=instance)
         try:
-            sample_list = TodoList.objects.get(id=12)
-            sample_list.users.add(instance)
+            now = datetime.date.today()
+            sample_list = TodoList.objects.create_list(user=instance, title='Welcome! Sample list')
+            sample_task = Todo.objects.create_todo(user=instance, title='This is a sample task.', list_slug=sample_list.slug)
+            sample_task = Todo.objects.create_todo(user=instance, title='This is a sample task with due date.', list_slug=sample_list.slug)
+            sample_task.due_date = now
+            sample_task.save()
+            sample_task = Todo.objects.create_todo(user=instance, title='This is a sample task with priority "critical". The default priority of a task is "Normal"', list_slug=sample_list.slug)
+            sample_task.priority = "critical"
+            sample_task.due_date = now
+            sample_task.save()
+            sample_task = Todo.objects.create_todo(user=instance, title='This is a sample task with priority "minor".', list_slug=sample_list.slug)
+            sample_task.priority = "minor"
+            sample_task.due_date = now
+            sample_task.save()
+            sample_task = Todo.objects.create_todo(user=instance, title='This is a sample task with description. Press edit(pencil icon below) to see the description.', list_slug=sample_list.slug)
+            sample_task.description = "Orayt! Rock 'n Roll to the world!"
+            sample_task.due_date = now
+            sample_task.save()
+            sample_task = Todo.objects.create_todo(user=instance, title='This is a sample task marked as done. Press the check icon to change status.', list_slug=sample_list.slug)
+            sample_task.status = "done"
+            sample_task.due_date = now
+            sample_task.save()
         except:
             pass
         # notify.send(
